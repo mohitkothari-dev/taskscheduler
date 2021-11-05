7e598ba6e36964b1480f7ddc111d4895210410a2
mohitkothari <mrulekothari@gmail.com>
2021-11-05 20:38:22 +0530

    My first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TASK SCHEDULER
+
+I have created a canvas abd tried implemeting some features with the p5 canvas.
+
+## Features implemeted so far
+* Image drag and drop functionality (still in initial phase)
+* Added the ability to change the color of the canvas
+* Delete the canvas
+* Can add text to the canvas
+
+### What features will be implemented next?
+* Drawing and Erasing
+* Collaboration
+
+### I will create a different branch for minimal features, not sure yet. 
---
+++ js/sketch.js
@@ -0,0 +1,43 @@
+let canvas;
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+let colorPicker;
+let button;
+
+let canvasWidth = width/2;
+let canvasHeight = height/2;
+
+let input;
+
+function setup() {
+    canvas = createCanvas(canvasWidth,canvasHeight); //built-in function from p5.js
+    createElement("br");
+    
+    button = createButton("delete");
+    button.position(0,canvasHeight + 10);
+    button.mousePressed(removeCanvas);
+
+    input = createInput('Add your task');       //adds input filed
+    input.position(button.width + 5, canvasHeight + 10);    //5px gap between the two
+        
+    colorPicker = createColorPicker('#EDEDED');
+    colorPicker.position(input.width + 50,canvasHeight + 10);
+    canvas.drop(droppedFile);
+}
+
+function removeCanvas() {
+    remove(); // remove whole sketch on mouse press
+} //we can execute remove() on delete button press {Global delete and local delete}
+
+function draw() {
+    background(colorPicker.color());
+    //text(input.value(),100,100);    //make it draw only once
+}
+
+function droppedFile(file) {
+    let img = createImg(file.data,'image');
+    img.size(100,100);
+    image(img,0,0,200,200);
+}
